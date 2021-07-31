c45572e9c66dfd8b8d6381b7cc053a56cdde2e93
Ahsan Iqbal <ahsaniqbal426@gmail.com>
2021-07-31 14:44:20 +0500

    practice link update

--- hideNshowPracriceSec.js
+++ hideNshowPracriceSec.js
@@ -11,14 +11,29 @@ $(function(){
     });
   });
 
-    // script for when click any area from index page it will show
+    // script for when click any link of practice from index page it will show
   
     $(document).ready(function(){
         if(window.location.hash){
             var current_area = document.querySelectorAll(window.location.hash)[0].id
             console.log(current_area);
             document.getElementById(current_area).style.display="block"; 
+            document.getElementsByTagName(`a[href='http://domain.com']`)
+            console.log( document.get);
         }else{
             document.getElementById('Litigation').style.display="block";
         }
     });
+
+// Add active-li class to the current button (highlight it)
+// var header = document.getElementById("practice-area-list");
+var btns =document.getElementsByClassName("show-active");
+console.log(btns);
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active-li");
+  console.log(document.getElementsByClassName("active-li"));
+  current[0].className = current[0].className.replace(" active-li", "");
+  this.className += " active-li";
+  });
+}
